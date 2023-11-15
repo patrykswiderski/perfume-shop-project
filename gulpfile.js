4eddcd7ffd7097eb9dc83508f3bc580788c60690
Patryk <patryk.swiderski.85@gmail.com>
2023-11-15 19:48:01 +0100

    Added SASS for next four sections

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'))
 // const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
-  return src('app/sass/index.scss')
+  return src('app/sass/*.scss')
     .pipe(sass())
     // .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['app/sass/index.scss'], buildStyles)
+  watch(['app/sass/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
